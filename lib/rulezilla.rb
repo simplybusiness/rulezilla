1f69dd8cc613846af1fb750850885198350022fa
PeterWuMC <peter.wu@simplybusiness.co.uk>
2014-08-13 13:09:22 +0100

    Rulezilla.set_gherkin_rules_path

--- lib/rulezilla.rb
+++ lib/rulezilla.rb
@@ -2,3 +2,14 @@ require 'rulezilla/node'
 require 'rulezilla/tree'
 require 'rulezilla/basic_support'
 require 'rulezilla/dsl'
+module Rulezilla
+
+  def self.set_gherkin_rules_path(path)
+    @gherkin_rules_path = path
+  end
+
+  def self.gherkin_rules_path
+    @gherkin_rules_path
+  end
+
+end
