f1d75c20e1ec7809bc1987493b0fef5adbbf4697
Jorja Hung <jorjahung@gmail.com>
2019-09-09 17:26:44 +0100

    Added semaphore id

--- _pipeline/config.sh
+++ _pipeline/config.sh
@@ -5,4 +5,4 @@
 # ------------------------------------------------------------------------------
 #
 # This gem is not yet on semaphore for some reason
-# export APP_SEMAPHORE_ID=''
+export APP_SEMAPHORE_ID='e488365d-9c57-4431-916a-72aea091d1b5'
